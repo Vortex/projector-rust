cbaa146afeb7cbb586709d013c07760f08627092
Peco Danajlovski <vortexmk@gmail.com>
2022-10-25 23:27:25 +0200

    implementations for get all values, get value, add value and remove value

--- src/projector.rs
+++ src/projector.rs
@@ -21,7 +21,55 @@ fn default_data() -> Data {
 }
 
 impl Projector {
-    fn from_config(config: Config) -> Self {
+    pub fn get_value_all(&self) -> HashMap<&String, &String> {
+        let mut curr = Some(self.config.pwd.as_path());
+        let paths = vec![];
+        while let Some(p) = curr {
+            paths.push(p);
+            curr = p.parent();
+        }
+
+        let out = HashMap::new();
+        for path in paths.iter().rev() {
+            if let Some(map) = self.data.projector.get(path) {
+                out.extend(map.iter())
+            }
+        }
+
+        return out;
+    }
+
+    pub fn get_value(&self, key: String) -> Option<&String> {
+        let mut curr = Some(self.config.pwd);
+        let mut out = None;
+
+        while let Some(p) = curr {
+            if let Some(dir) = self.data.projector.get(p) {
+                if let Some(value) = dir.get(&key) {
+                    out = Some(value)
+                }
+            }
+            curr = p.parent();
+        }
+    }
+
+    pub fn set_value(&self, key: String, value: String) {
+        self.data
+            .projector
+            .entry(self.config.pwd)
+            .or_default()
+            .insert(key, value);
+    }
+
+    pub fn remove_value(&self, key: &str) {
+        self.data
+            .projector
+            .entry(self.config.pwd)
+            .or_default()
+            .remove(key);
+    }
+
+    pub fn from_config(config: Config) -> Self {
         // if std::fs::try_exists(config.config).is_ok() {
         // }
         if std::fs::metadata(config.config).is_ok() {
